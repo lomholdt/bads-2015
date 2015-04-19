f282bd113e87bbcee90ab005ab719e6c127b2102
jonaslomholdt <jonaslomholdt@gmail.com>
2015-04-19 16:30:51 +0200

    added 8

---
+++ 8/code/SpanningUSA.java
@@ -0,0 +1,95 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SpanningUSA{
+
+	private static Map<String, Integer> citiesToInt = new HashMap<>();
+	private static Map<Integer, String> intToCities = new HashMap<>();
+	private static EdgeWeightedGraph graph;
+
+	public static void main(String[] args) {
+
+        if(!StdIn.isEmpty()){
+	        Pattern edges = Pattern.compile("(?m)^(\\w+.?\\w*)$");
+	        Pattern edgePattern = Pattern.compile("(?!\")\\w+\\s*\\w*[^\"-\\[\\]]?");
+
+        	int count = 0;
+        	while(StdIn.hasNextLine()){ // Add vertices to map
+        		String input = StdIn.readLine().trim();
+        		Matcher e = edges.matcher(input);
+
+        		
+        		if(!input.contains("[")){
+        			StdOut.println(input);
+        			citiesToInt.put(input, count);
+        			intToCities.put(count, input);
+        			count++;
+        		}
+        		else{
+        			break;
+        		}
+        		
+        	}
+        	graph = new EdgeWeightedGraph(citiesToInt.size());
+        	
+        	int index = 0;
+        	while(StdIn.hasNextLine()){
+        		String input = StdIn.readLine();
+        		// Matcher ep = edgePattern.matcher(input);
+        		// while(ep.find()){
+        		// 	StdOut.print("Group: " + ep.group());
+        		// }
+
+        		StdOut.println(input);
+        		String weightStr = input.substring(input.indexOf("[")+1 , input.indexOf("]"));
+        		StdOut.println(weightStr);
+
+                String cities = input.substring(0 , input.indexOf("[")-1 );
+                StdOut.println(cities);
+                String  first = cities.substring(0 , input.indexOf("--"));
+                String  second = cities.substring(input.indexOf("--")+2 , cities.length());
+                // first = first.replace("\"", "");
+                // second = second.replace("\"", "");
+                Double weight = Double.parseDouble(weightStr);
+
+
+    			// String[] s = input.split("--");
+    			// String[] sa = s[1].split(" ");
+
+    			// String first = s[0];
+    			// String second = sa[0];
+    			// String weightString = sa[1].substring(1, sa[1].length()-1);
+    			// Double weight = Double.parseDouble(weightString+".0");
+    			StdOut.println(first);
+    			StdOut.println(second);
+    			StdOut.println(weight);
+
+				Edge e = new Edge(citiesToInt.get(first), citiesToInt.get(second), weight);
+				graph.addEdge(e);
+    			
+    			StdOut.println(String.format("%s - %s w: %s", first, second, weight));
+        	}
+			StdOut.println(graph);
+
+			KruskalMST mst = new KruskalMST(graph);
+
+			double weightSum = 0.0;
+			for (Edge e : mst.edges()) {
+				int val1 = e.either();
+				int val2 = e.other(val1);
+
+				String cityA = intToCities.get(val1);
+				String cityB = intToCities.get(val2);
+				Double weight = e.weight();
+				weightSum += weight;
+
+				StdOut.println(String.format("%s - %s : %1.2f", cityA, cityB, weight));
+			}
+			StdOut.println(weightSum);
+		}
+	}
+}
